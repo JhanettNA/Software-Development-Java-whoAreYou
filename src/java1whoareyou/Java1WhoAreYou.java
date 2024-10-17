fe9e9531a874e9f98c8d01aa4ddddb2775762e9d
Jhanett <jhanettsis@gmail.com>
2024-10-17 20:12:08 +0100

    Initial commit

--- src/java1whoareyou/Java1WhoAreYou.java
+++ src/java1whoareyou/Java1WhoAreYou.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package java1whoareyou;
 /**
  * I import the Scanner class to allow reading input from the user.
